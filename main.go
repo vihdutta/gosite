7b0c3502f5f1f2e74f388044c7ec719221043990
vihdutta <vihdutta@gmail.com>
2021-03-21 11:33:27 -0400

    Added "projects.html"

--- main.go
+++ main.go
@@ -4,33 +4,28 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"time"
 )
 
-type Welcome struct {
-	Name string
-	Time string
-}
-
 func main() {
-	//welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
-	//templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
-
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	http.HandleFunc("/", home)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/projects", projects)
 	fmt.Println(http.ListenAndServe(":6969", nil))
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
-	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
+func index(w http.ResponseWriter, r *http.Request) {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
 
-	if name := r.FormValue("name"); name != "" {
-		welcome.Name = name
+	if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
 
-	if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
+func projects(w http.ResponseWriter, r *http.Request) {
+	templates := template.Must(template.ParseFiles("templates/projects.html"))
+
+	if err := templates.ExecuteTemplate(w, "projects.html", nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
